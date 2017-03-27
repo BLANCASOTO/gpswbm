3ecf2bc112e4e321faecf55da97b4caf0e73fd0f
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-03-27 07:45:33 -0600

    Add files via upload

--- php/conexion.php
+++ php/conexion.php
@@ -1,24 +1,24 @@
 <?php
-	/*$sesion = "cerrada";
+	$sesion = "cerrada";
 	$puerto = "edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
 	$usuario = "t175x1vm0e1cnb9d";
 	$contrasena = "i1k0acj2kb0lyi4w";
 	$db_name = "ldfhtvvunzogr8mu";
 	$db_host = $puerto;
 	$db_user = $usuario;
-	$db_password = $contrasena;*/
+	$db_password = $contrasena;
 
 //mysql -h edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com -u t175x1vm0e1cnb9d -p
 
 	
-	$sesion = "cerrada";
+	/*$sesion = "cerrada";
 	$puerto = "localhost";
 	$usuario = "root";
 	$contrasena = "1234";
 	$db_name = "gpswbm";
 	$db_host = $puerto;
 	$db_user = $usuario;
-	$db_password = $contrasena;
+	$db_password = $contrasena;*/
 	
 
 ?>
