d3400ad93c02a0ffc26f70b91409803c3fa8e5ce
MarGaryIto <1715110196@utec-tgo.edu.mx>
2017-03-31 09:13:22 -0600

    Update conexion.php

--- php/conexion.php
+++ php/conexion.php
@@ -1,12 +1,18 @@
 <?php
 	$sesion = "cerrada";
-	$puerto = "edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
-	$usuario = "t175x1vm0e1cnb9d";
-	$contrasena = "i1k0acj2kb0lyi4w";
-	$db_name = "ldfhtvvunzogr8mu";
+	$puerto = "us-iron-auto-dca-03-a.cleardb.net";
+	$usuario = "ba7b01d9752c93";
+	$contrasena = "8a6b1ffc";
+	$db_name = "heroku_d0f1fee03002369";
+	
 	$db_host = $puerto;
 	$db_user = $usuario;
 	$db_password = $contrasena;
+
+	$server = $url[$puerto];
+	$username = $url[$usuario];
+	$password = $url[$contrasena];
+	$db = substr($url[$db_name], 1);
 
 //mysql -h edo4plet5mhv93s3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com -u t175x1vm0e1cnb9d -p
 
